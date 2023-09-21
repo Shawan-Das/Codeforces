eebe0bb96f321c3caf2e39b129432a7bb873ed46
Shawan Das <shawon91221@outlook.com>
2023-09-21 10:08:29 +0600

    Runtime Error

--- code1.py
+++ code1.py
@@ -1,51 +1,58 @@
-test=int(input())
+test = int(input())
 
 for t in range(test):
     n=int(input())
-    data=list(map(int, input().split(" ")))
-    numbers=[1]*n
-    numbers=[0]+numbers
+    data=list(map(int,  input().split(" ")))
+    
     total= (n*(n+1))//2
+    
     if(data[-1]>total):
         print("NO")
         continue
     
-    if(total != data[-1]): 
+    if(data[-1] < total):
         data.append(total)
-        
-        if(numbers[data[0]] == 0 or numbers[data[0]-1]>n):
-            print("NO")
-            continue
-        numbers[data[0]]=0    
-        for i in range(1,n):
-            var = data[i]-data[i-1]
-            if(numbers[var] ==1): numbers[var]=0
-            else: break
-        if(sum(numbers)==0): print("YES")
+        diff=[data[i]-data[i-1] for i in range(1,n)]
+        diff.insert(0,data[0])
+        if(sum(set(diff))==total): print("YES")
         else: print("NO")
         continue
     
-    diff=[]
-    
-    for i in range(len(data)):
-        if(i==0): continue
-        else: 
-            var= data[i]-data[i-1]
-            # print("--",var,'--')
-            # print("--",numbers,"--")
+    else:
+        number=[1]*n
+        number.insert(0,0)
+        diff=[]
+        flag=False
+        for i in range(len(data)):
+            if(i==0): continue
+            
+            var=data[i]-data[i-1]
+            
+            if(var==0): 
+                flag=True
+                break
             if(var>n): diff.append(var)
-            elif(numbers[var]==1): numbers[var]=0 
-            else: diff.append(var)
-    if(len(diff)==0): diff.append(data[0])
-    elif(numbers[data[0]] ==1 ): numbers[data[0]] =0
-    if(sum(numbers)>2):
-        print("NO")
-        continue
-    elif(len(diff)>1): 
-        print("2NO")
-        continue
-    g1=numbers.index(1)
-    g2=numbers.index(1,g1+1)
-    if(g1+g2==diff[0]): print("YES")
-    else: print("3NO")
-    
+            elif(number[var]==1): number[var]=0
+            elif(number[var]==0): diff.append(var)
+            
+        if(flag):
+            print("NO")
+            continue
+        if(len(diff)==0): diff.append(data[0])
+        else: number[data[0]]=0
+        #print(number)
+        if(sum(number)!=2):
+            print("NO")
+            continue
+        if(len(diff)>1):
+            print("NO")
+            continue
+        
+        g1= number.index(1)
+        g2= number.index(1,g1+1)
+        
+        if(g1+g2==diff[0]): print("YES")
+        else: print("NO")
+        
+        data.clear()
+        number.clear()
