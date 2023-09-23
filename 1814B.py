9dff2438c95133d61e32e156730bcf7e120670b4
Shawan Das <shawon91221@outlook.com>
2023-09-23 10:00:00 +0600

    time limit

--- 1814B.py
+++ 1814B.py
@@ -1,4 +1,10 @@
-import math
-m,n=10,100
-print(math.gcd(m,n))
-#print(math.lcm(m,n))
+test= int(input())
+
+for t in range(test):
+    a,b = map(int, input().split(" "))
+    
+    result = a+b
+    for m in range(1,100001):
+        result= min(result, (a + m - 1) // m + (b + m - 1) // m + (m - 1))
+    
+    print(result)
