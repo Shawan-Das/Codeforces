74f6c86f7620b7ec631410f3de9d8ec4677fb0f0
Shawan Das <shawon91221@outlook.com>
2023-09-24 16:30:58 +0600

    1806A unsolved

--- code1.py
+++ code1.py
@@ -1,11 +1,14 @@
-for t in range(int(input())):
-    x,k= map(int,input().split(" "))
-    data=[]
+x,y=0,1
 
-    if(k>x): data=[x]
-    elif(x%k != 0): data=[x]
-    else: data=[1,x-1]
-
-    print(len(data))
-    for d in data: print(d,end=" ")
-    print()
+for t in range(int(input())): 
+    data=list(map(int,input().split(' ')))
+    a,b=data[:2],data[2:]
+    if(a[x]==b[x] and a[y]==b[y]): step=0
+    elif(a[y]==b[y] and b[x]<a[x]): step= a[x]-b[x]
+    else:
+        step= b[y]-a[y]
+        a[x]+= step
+        a[y]+= step
+        if(a[x]>b[x]): step+= a[x]-b[x]
+        else: step=-1
+    print(step)
