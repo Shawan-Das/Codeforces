65cd0e8fdb4b0eff91871c850b96cb57961f859f
Shawan Das <shawon91221@outlook.com>
2023-09-24 12:34:03 +0600

    In queue

---
+++ 1846A.py
@@ -0,0 +1,9 @@
+a,b=0,1
+for t in range(int(input())):
+    data=[]
+    for i in range(int(input())):
+        data.append(list(map(int,input().split(" "))))
+    
+    count=[d for d in data if d[a]>d[b]]
+    
+    print(len(count))
