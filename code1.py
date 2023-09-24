4ca274fb079befc059f654de47759ed2fa21367d
Shawan Das <shawon91221@outlook.com>
2023-09-24 10:11:07 +0600

    1845B incomplete

--- code1.py
+++ code1.py
@@ -1,21 +1,42 @@
-for t in range(int(input())):
-    v=input()
-    
-    if(v[0]=='0'):
-        print('0')
-        continue
-    elif(len(v)==1):
-        if(v=="?"): print('9')
-        elif(v=='0'): print('0')
-        else: print('1')
-        continue
-    count=1
-    mark= v.count('?')
-    
-    if(v[0]=='?'): count,mark = 9, mark-1
-    
-    total = count * (10**mark)
-    
-    print(total)
-        
-        
+def samePoint(a,x):
+    if(a[0]==x[0] and a[1]==x[1]): return True
+    else: return False
+
+def left_right(a,x):
+    if(x[0]>a[0]): return 'right'
+    else: return 'left'
+
+def up_down(a,x):
+    if(x[1]>a[1]): return 'up'
+    else: return 'down'
+
+def sameX(a,x):
+    if(a[0]==x[0]): return True
+    else: return False
+
+def sameY(a,y):
+    if(a[1]==y[1]): return True
+    else: return False
+
+
+x,y,count=0,1,"something else"
+A= list(map(int,input().split(" ")))
+B= list(map(int,input().split(" ")))
+C= list(map(int,input().split(" ")))
+
+if(samePoint(A,B) or samePoint(A,C)): count=1  # same point
+
+elif(sameX(A,B) and sameX(A,C)): # same X position
+    if(up_down(A,B) != up_down(A,C)) : count=1
+    elif(up_down(A,B)=='up'): count= abs(A[y]-min(B[y],C[y]))+1
+    else: count= abs(A[y]-max(B[y],C[y]))+1
+
+elif(sameY(A,B) and sameY(A,C)): # same Y position
+    if(left_right(A,B) != left_right(A,C)) : count=1
+    elif(left_right(A,B)=='right'): count= abs(A[x]-min(B[x],C[x]))+1
+    else: count= abs(A[x]-max(B[x],C[x]))+1
+
+
+# elif(xa==xb and xc==xb): count= abs(ya-min(yb,yc))+1
+# elif((xa==xb and ya==yb) or (xa==xc and ya==yc)): count=1
+# elif(() or ())
